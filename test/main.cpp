6e5b4f9f416da47695b219b6e366e8707258a411
Will Fisher <will@gina.alaska.edu>
2015-04-06 13:57:27 -0800

    adding catch test framework

---
+++ test/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Will Fisher on 4/6/15.
+//  Copyright (c) 2015 Will Fisher. All rights reserved.
+//
+
+#include <iostream>
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+TEST_CASE( "Shapes", "[shape]")
+{
+  SECTION( "default constructor" )
+  {
+    
+  }
+}
